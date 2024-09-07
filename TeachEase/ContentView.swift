5e3b302e28ef3a8a2b7d2fda7684e3820c9c49bd
Kanchan.Kaur <romalkanchandeepkaur@gmail.com>
2024-09-06 20:44:49 -0400

    Initial Commit

---
+++ TeachEase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeachEase
+//
+//  Created by Kanchan Kaur on 2024-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeachEase/TeachEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeachEaseApp.swift
+//  TeachEase
+//
+//  Created by Kanchan Kaur on 2024-09-06.
+//
+
+import SwiftUI
+
+@main
+struct TeachEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
